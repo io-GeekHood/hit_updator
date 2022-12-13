1ac47d2202c59e3d4099dbc2ad3e818a44c4b40c
FeedMeUn1C0de <hashempourian.a@gmail.com>
2022-12-13 00:45:51 -0500

    fix: debug in if statement

--- producer.py
+++ producer.py
@@ -95,7 +95,7 @@ def to_timestamp(oktime:str):
     return round(gregorian)
 def transformer(data:dict) -> dict:
     if data["brand"] is None or data["brand"] == "null":
-        data["brand"]["createdOn"] = "1396/9/7 20:01"
+        data["brand"] = {"createdOn":"1396/9/7 20:01"}
 
 
     message = {
